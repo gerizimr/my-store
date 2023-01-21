b5c711b947840b3419fa2d6fadf440faed5b3cda
gerizimr <sinaicisco1193@gmail.com>
2023-01-20 22:39:55 -0600

    fundamentos de angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { products } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,92 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  widthImg = 10;
+  name = 'Sinai';
+  age = 18;
+  img = 'https://images.unsplash.com/photo-1483884105135-c06ea81a7a80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80';
+  btnDIsabled = true;
+  register = {
+    name: '',
+    email: '',
+    password: '',
+  }
+  person = {
+    name: 'Sinai',
+    age: 18,
+    avatar: 'https://images.unsplash.com/photo-1483884105135-c06ea81a7a80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80'
+  }
+  names: string[] = ['Nico', 'Juli', 'Santi'];
+  newName = '';
+  box = {
+    width: 100,
+    height: 100,
+    background: 'red'
+  };
+  products: products[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: './assets/images/house.jpg'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: './assets/images/glasses.jpg'
+    }
+  ]
+
+  toggerButton() {
+    this.btnDIsabled = !this.btnDIsabled;
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+
+  }
+
+  addName() {
+    this.names.push(this.newName);  // para agregar un nuevo nombre a la lista
+    this.newName = '';   // para limpiar el input
+  }
+
+  deleteName(index: number) {  // para eliminar los elementos de la lista
+    this.names.splice(index, 1);  // splice para eliminar los elementos
+  }
+
+  onRegister() {
+    console.log(this.register);
+  }
+
 }
---
+++ src/app/product.model.ts
@@ -0,0 +1,6 @@
+export interface products {
+  name: string;
+  price: number;
+  image: string;
+  category?: string;  // con esto indicamos que category es opcional no obligatorio.
+}
